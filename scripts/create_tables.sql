94f83064e1d2e0b6213ace2507614a1d7ebee972
k0pernicus <dev@0xc0ff33.me>
2022-02-19 15:46:58 +0100

    Missed coordinates in Annotations table

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,6 +1,10 @@
 CREATE TABLE IF NOT EXISTS ANNOTATIONS (
     id               SERIAL   PRIMARY KEY,
     content          TEXT     NOT NULL,
+    x                INTEGER  NOT NULL CHECK (x <= x2 AND x >= 0),
+    x2               INTEGER  NOT NULL CHECK (x2 >= x AND x2 >= 0),
+    y                INTEGER  NOT NULL CHECK (y <= y2 AND y >= 0),
+    y2               INTEGER  NOT NULL CHECK (y2 >= y AND y2 >= 0),
     created_at       TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
